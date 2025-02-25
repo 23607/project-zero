41220349c26a266fe10fddf03ea35f4709fd8dbd
23607 <23607@fraser.school.nz>
2025-02-26 11:45:07 +1300

    Update test.py
    Poblems solved. The function now works as planned after changing some values to numbers and rounding them

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 def decorate_text(boring_txt, decoration, lines):
     """Creates headings (3 lines), subheadings (2 lines) and
     emphasised text / mini-headings (1 line). Emojis are recomended only for emphasised text"""
-
+    blines = int(lines)
     middle = f"{decoration * 2} {boring_txt} {decoration * 2}"
     top_bottom = decoration * len(boring_txt)
 
@@ -12,9 +12,9 @@ def decorate_text(boring_txt, decoration, lines):
         print(top_bottom)
 
     else:
-        print(top_bottom * lines / 2)
+        print(round(blines / 2) * top_bottom)
         print(middle)
-        print(top_bottom * lines / 2)
+        print(round(blines / 2) * top_bottom)
 
 # Main Routine goes here
 decorate_text("a big title!", "⭐", 6)
