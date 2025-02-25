a365341f3a5219868da1b64fdfc7e19a9f39aa88
23607 <23607@fraser.school.nz>
2025-02-26 11:45:35 +1300

    Update Component1=Txt_deco.py
    Compopnent updated

--- Component1=Txt_deco.py
+++ Component1=Txt_deco.py
@@ -1,9 +1,24 @@
-# Functions
-def decorate_text(boring_txt, decoration):
-    """Emphasises headings by adding decoration
-    at the start and end"""
+def decorate_text(boring_txt, decoration, lines):
+    """Creates headings (3 lines), subheadings (2 lines) and
+    emphasised text / mini-headings (1 line). Emojis are recomended only for emphasised text"""
+    blines = int(lines)
+    middle = f"{decoration * 2} {boring_txt} {decoration * 2}"
+    top_bottom = decoration * len(boring_txt)
 
-    print(f"{decoration * 2} {boring_txt}{decoration * 2}")
+    if lines == 1:
+        print(middle)
+    elif lines == 2:
+        print(middle)
+        print(top_bottom)
 
-# Main Routine
-decorate_text("Programming is Fun!", "⭐")
+    else:
+        print(round(blines / 2) * top_bottom)
+        print(middle)
+        print(round(blines / 2) * top_bottom)
+
+# Main Routine goes here
+decorate_text("a big title!", "⭐", 6)
+print()
+decorate_text("12345671234567", "⭐", 2)
+print()
+decorate_text("look at me!", "⭐", 1)
