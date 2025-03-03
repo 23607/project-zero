4135009861d85510a55c60c18b011692b178126c
23607 <23607@fraser.school.nz>
2025-03-04 10:21:51 +1300

    Component 2 updated
    The component was expanded with another functiopn, but it has some errors

--- Component2=No_blank_fields.py
+++ Component2=No_blank_fields.py
@@ -1,8 +1,14 @@
 #Functions
+def heatmor (fild):
+    '''This will detect if there is any number in a field'''
+    return any(num.isdigit() for num in fild)
+
 def no_blank_fields_pls (field):
     '''If the user does not fill a field, this function will detect it and ask them to do it'''
-    if field == '':
-        print("This field can't be blank. Please, fill it and try again.")
+    bfield = heatmor(field)
+    while field == '' or bfield == True:
+        print("This field can't be blank or have numbers in it. Please try again.")
+        field = input("Answer here: ")
 #Main
 a = input("What's your name? ")
 no_blank_fields_pls(a)
