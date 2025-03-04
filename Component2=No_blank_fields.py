2b7bf729d8badd09c49416c9014784947fa6c778
23607 <23607@fraser.school.nz>
2025-03-05 11:31:05 +1300

    Component2 bugs fixed.
    the 'Heatmor' function was deleted and replaced by string.isalpha. this function was also added to the 'while' loop to fix the last bug that prevented the user from getting out of the loop.

--- Component2=No_blank_fields.py
+++ Component2=No_blank_fields.py
@@ -1,14 +1,11 @@
 #Functions
-def heatmor (fild):
-    '''This will detect if there is any number in a field'''
-    return any(num.isdigit() for num in fild)
-
 def no_blank_fields_pls (field):
     '''If the user does not fill a field, this function will detect it and ask them to do it'''
-    bfield = heatmor(field)
-    while field == '' or bfield == True:
+    bfield = field.isalpha()
+    while bfield == '' or bfield == False:
         print("This field can't be blank or have numbers in it. Please try again.")
         field = input("Answer here: ")
+        bfield = field.isalpha()
 #Main
 a = input("What's your name? ")
 no_blank_fields_pls(a)
