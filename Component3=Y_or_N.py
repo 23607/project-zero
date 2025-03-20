a4f2f3d5beae460f3743dda5d0193b6e9ac7bb5b
23607 <23607@fraser.school.nz>
2025-03-20 13:22:37 +1300

    Component 3 fixed and expanded
    Fixed an error that took all answers as yes or no. Added a break so the user can break the loop, and if the user does not answer yes or no, the program tells the user to enter a second answer.

--- Component3=Y_or_N.py
+++ Component3=Y_or_N.py
@@ -1,18 +1,25 @@
 # functions
 def Y_or_N(question):
     """Checks that users enter yes / y or no / n to a question"""
-    question = question.lower()
+    questionb = question.lower()
     while True:
-        if question == 'yes' or 'y':
+        if questionb == 'yes' or questionb == 'y':
             return 'yes'
-        elif question == 'no' or 'n':
+        elif questionb == 'no' or questionb == 'n':
             return 'no'
+        elif questionb == 'stop':
+            print('The program has been stopped')
+            break
         else:
             print("Please enter 'yes', 'y', 'no' or 'n'. ")
+            question = input('answer again here, please: ')
+            questionb = question.lower()
 
 # Main
 george = input('Do you like ice cream? ')
 bee = Y_or_N(george)
 (print)
 if bee == 'yes':
+    print('This works!')
+if bee == 'no':
     print('This works!')
