9bf0de9c16a455026d37b5e5854d02b7284f25d0
23607 <23607@fraser.school.nz>
2025-03-25 10:18:58 +1300

    Component 2 used to check the name introduced by the user
    I'm currently working on a loop that will allow the user to choose their name and re-write it if they misspelled it or change their mind. Component2 will make sure the user is introducing an actual name.

--- Component2=No_blank_fields.py
+++ Component2=No_blank_fields.py
@@ -3,8 +3,8 @@ def no_blank_fields_pls (field):
     '''If the user does not fill a field, this function will detect it and ask them to do it'''
     bfield = field.isalpha()
     while bfield == '' or bfield == False:
-        print("This field can't be blank or have numbers in it. Please try again.")
-        field = input("Answer here: ")
+        print(">Emm, I think you should answer with words.<")
+        field = input("Answer again here please: ")
         bfield = field.isalpha()
 #Main
 a = input("What's your name? ")
--- Main_Program.py
+++ Main_Program.py
@@ -17,6 +17,14 @@ def Y_or_N(question):
             print("Please enter 'yes', 'y', 'no' or 'n'. ")
             question = input('>Psst, player, the question was yes or no, answer again here, please<: ')
             questionb = question.lower()
+    
+def no_blank_fields_pls (field):
+    '''If the user does not fill a field, this function will detect it and ask them to do it'''
+    bfield = field.isalpha()
+    while bfield == '' or bfield == False:
+        print(">Emm, I think you should answer... with words :D.<")
+        field = input("Answer again here please: ")
+        bfield = field.isalpha()
 
 #Main code
 print("Hello there! It's so very nice to meet you! Welcome to the world of Pokémon!")
@@ -35,7 +43,16 @@ print(">POP! A Pokémon came out of the Pokeball!<")
 time.sleep(1.5)
 name_correct = False
 while name_correct == False:
-    if name_correct == False:
-        pname = input("Tell me, what is your name? ")
-#"pname" is the player's name. This will be used later on
-        input(f"So, Ok... you're {pname}?")
+    pname = input("Tell me, what is your name? ")
+    no_blank_fields_pls(pname)
+#"pname" is the player's name. This will be used later on.
+    time.sleep(0.5)
+    confirm_name = input(f"So, Ok... you're {pname}?")
+    Y_or_N(confirm_name)
+    (print)
+    if Y_or_N == 'yes':
+        name_correct = True
+    elif Y_or_N == 'no':
+        time.sleep(1)
+        print("Oh, ok.")
+            
